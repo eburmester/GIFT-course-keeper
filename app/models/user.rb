be752223e8723549b24e6560054394fb47ec3456
eburmester <burmesterelyse@gmail.com>
2019-03-25 10:09:32 -0700

    ebur

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ActiveRecord::Base
     has_many :user_courses
-    has_many :courses, through: :user_courses 
+    has_many :shared_courses, through: :user_courses, source: "course"
 end
--- app/models/user_course.rb
+++ app/models/user_course.rb
@@ -1,4 +1,4 @@
-class UserCourses < ActiveRecord::Base
+class UserCourse < ActiveRecord::Base
     belongs_to :user 
     belongs_to :course 
 end
